86571b6880cd489c2e1b0fc995900065a2b734ef
rohithvudayana <93068031+rohithvudayana@users.noreply.github.com>
2023-08-16 19:29:49 +0530

    sorted based on given conditions

--- Question1/server.js
+++ Question1/server.js
@@ -6,10 +6,11 @@ const app = express();
 
 app.get("/trains", async (req, res) => {
     try {
-        const accessCode = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTIxOTM1NzUsImNvbXBhbnlOYW1lIjoiVHJhaW4gQ2VudHJhbCIsImNsaWVudElEIjoiOTlmODcwNjQtOTg2NS00NjhhLTk2ODctZTkxZWZkYjM1MTYxIiwib3duZXJOYW1lIjoiIiwib3duZXJFbWFpbCI6IiIsInJvbGxObyI6IlMyMDIwMDAyMDMxMyJ9.r3GdDi2nmLlo19xQQxPPULge-gkEflJt3hoH3sPENY8';
+        const accessCode = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTIxOTQzNTIsImNvbXBhbnlOYW1lIjoiVHJhaW4gQ2VudHJhbCIsImNsaWVudElEIjoiOTlmODcwNjQtOTg2NS00NjhhLTk2ODctZTkxZWZkYjM1MTYxIiwib3duZXJOYW1lIjoiIiwib3duZXJFbWFpbCI6IiIsInJvbGxObyI6IlMyMDIwMDAyMDMxMyJ9.X_j-Hhue2CDeBJlrtHArH7yVXyF0iGmezDXRucEskk4';
         const headers = { Authorization: `Bearer ${accessCode}` };
         const result = await axios.get("http://20.244.56.144/train/trains", { headers })
-        let data = result.data
+        let data = Object.values(result.data)
+        // console.log(data);
 
         // Ignoring trains in next 30 minutes
 
@@ -30,7 +31,28 @@ app.get("/trains", async (req, res) => {
         
 
 
-        // 
+        // Sorting Trains
+
+        data.sort((train1,train2)=>{
+            // increasing order of prices
+            if(train1.price.AC !== train2.price.AC){
+                return train1.price.AC - train2.price.AC
+            }
+            else if(train1.price.sleeper !== train2.price.sleeper){
+                return train1.price.sleeper - train2.price.sleeper
+            }
+            
+            // decreasing order of tickets
+            if(train1.seatsAvailable.AC !== train2.seatsAvailable.AC){
+                return train2.seatsAvailable.AC - train1.seatsAvailable.AC
+            }
+            
+            //decreasing order of departure time considering delay
+            const departtime1 = 60*train1.departureTime.Hours+train1.departureTime.Minutes+train1.delayedBy
+            const departtime2 = 60*train2.departureTime.Hours+train2.departureTime.Minutes+train2.delayedBy
+            
+            return departtime2 - departtime1
+        })
 
 
 
